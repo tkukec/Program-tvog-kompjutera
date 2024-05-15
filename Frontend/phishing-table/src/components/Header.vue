df3d342909fc0e5fd292b1a55de1e6ae6aeb3b70
dominikcuric <dominikcuric00@gmail.com>
2024-05-15 21:11:52 +0200

    Added layout page and header

---
+++ Frontend/phishing-table/src/components/Header.vue
@@ -0,0 +1,35 @@
+<!-- Header.vue -->
+<template>
+    <header class="navbar navbar-expand-lg navbar-dark bg-primary h-100">
+      <div class="container">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <router-link to="/" class="nav-link">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/dashboard" class="nav-link">Dashboard</router-link>
+            </li>
+          </ul>
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <router-link to="/login" class="nav-link">Login</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/register" class="nav-link">Register</router-link>
+            </li>
+          </ul>
+      </div>
+    </header>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Header'
+  }
+  </script>
+  
+  <style>
+  .navbar .nav-link {
+    font-size: 22px;
+  }
+</style>
